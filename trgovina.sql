63377d25d1f8cb5cd5d59ac99b2f9b58e09439d3
natasaklajic <nateabach@gmx.net>
2025-05-09 19:19:39 +0200

    Update trgovina.sql

--- trgovina.sql
+++ trgovina.sql
@@ -10,11 +10,11 @@
 --create table proizvodi(
 --sifra int,
 --naziv varchar(50),
---cijena decimal(18,2),
+--cijena decimal(18,2)
 --);
 
 --create table stavke (
 --racun varchar (20),
 --proizvod varchar (20),
---kolicina int,
+--kolicina int
 --);
