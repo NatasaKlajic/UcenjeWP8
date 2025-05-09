5ed04d105aa1554e02fdfbdd262553135f3073a1
natasaklajic <nateabach@gmx.net>
2025-05-09 19:36:11 +0200

    Update frizerskisalon.sql

--- frizerskisalon.sql
+++ frizerskisalon.sql
@@ -7,3 +7,13 @@
 --prezime varchar (30),
 --OIB int
 --);
+
+--create table korisnik (
+--sifra int,
+--ime varchar (30),
+--prezime varchar (30),
+--kontakt varchar (30),
+--vrijemeposjeta datetime 
+--);
+
+
