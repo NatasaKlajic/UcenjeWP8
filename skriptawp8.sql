4bbc3d913e1cdea3d466bd51f928ae67fcd71f5d
natasaklajic <nateabach@gmx.net>
2025-05-09 18:39:56 +0200

    Update skriptawp8.sql

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -2,13 +2,14 @@
 -- drop database edunovawp8;
 -- use edunovawp8;
 
-create table smjerovi(
-sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-datumpokretanja datetime,
-aktivan bit
-);
+--create table smjerovi(
+--sifra int,
+--naziv varchar(50),
+--cijena decimal(18,2),
+--datumpokretanja datetime,
+--aktivan bit
+--);
+
 --create table grupe(
 --sifra int,
 --naziv varchar(20),
@@ -16,4 +17,14 @@ aktivan bit
 --predavac varchar(50)
 --);
 
+--create table polaznici(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--email varchar(100)
+--);
 
+--create table clanovi(
+--grupa int,
+--polaznik int
+--)
